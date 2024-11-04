25305dc50018a31955577f3880ebb6ba50ce0417
sossaii <160123244+sossaii@users.noreply.github.com>
2024-11-04 11:44:44 -0300

    Add files via upload

---
+++ assets/img/main.js
@@ -0,0 +1,19 @@
+function criaCartao(tema,pergunta,resposta){
+const container = document.getElementById('container');
+let cartao = document.createElement('article');
+cartao.className =  'cartao';
+cartao.innerHTML =`
+<div class="cartao-conteudo">
+<h3>${tema}</h3>
+<div class="cartao-conteudo-pergunta">
+    ${pergunta}
+</div>
+<div class="cartao-conteudo-resposta">
+    ${resposta}
+</div>
+</div>
+`
+container.appendChild(cartao)
+}
+criarCartao('historia','em que ano iniciou-se a guerra do vietnã?','Em 1955')
+criarCartao('portugues','oque é paroxitona?','uma paroxitona é uma palavra cuja silaba tonica é penultima')
